d6c137c962e57a2e6a422fbabd2bf6c2874e4265
Hailey Zweedyk <haileyzweedyk@Haileys-MBP-2.lan>
2025-11-21 11:50:48 -0500

    Firebase connected

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,16 +1,20 @@
+// Import the functions you need from the SDKs you need
 import { getFirestore } from "firebase/firestore";
 import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  //   // COPY this from your Firebase Console
-  //   apiKey: "your-api-key-goes-here",
-  //   authDomain: "your-project-name-here.firebaseapp.com",
-  //   databaseURL: "https://your-project-name-here.firebaseio.com",
-  //   projectId: "your-project-name-here",
-  //   storageBucket: "your-project-name.appspot.com",
-  //   messagingSenderId: "xxxxxxxx",
+  apiKey: "AIzaSyAX1iPxLR8oHM_06hz5YZpV89mCr2cCPTM",
+  authDomain: "cis371-f015e.firebaseapp.com",
+  projectId: "cis371-f015e",
+  storageBucket: "cis371-f015e.firebasestorage.app",
+  messagingSenderId: "652231514148",
+  appId: "1:652231514148:web:8dd98ca8bec905791260b1"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
