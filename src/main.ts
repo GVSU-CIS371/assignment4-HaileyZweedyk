3092cb9c8e96f088db52691660f17eb7bd320591
Hailey Zweedyk <haileyzweedyk@Haileys-MBP-2.lan>
2025-11-21 12:24:25 -0500

    Loaded bases to beverageStore.ts and declared init before mounting the app in main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,5 +5,10 @@ import piniaPluginPersistedState from "pinia-plugin-persistedstate";
 import App from "./App.vue";
 const pinia = createPinia();
 pinia.use(piniaPluginPersistedState);
+import { useBeverageStore } from "./stores/beverageStore";
 
-createApp(App).use(pinia).mount("#app");
+const beverageStore = useBeverageStore();
+
+beverageStore.init().then(() => {
+  createApp(App).use(pinia).mount("#app");
+});
--- src/stores/beverageStore.ts
+++ src/stores/beverageStore.ts
@@ -17,6 +17,7 @@ import {
   onSnapshot,
 } from "firebase/firestore";
 
+
 export const useBeverageStore = defineStore("BeverageStore", {
   state: () => ({
     temps: tempretures,
@@ -33,7 +34,24 @@ export const useBeverageStore = defineStore("BeverageStore", {
   }),
 
   actions: {
-    init() {},
+    async init() {
+      // Load bases
+      const basesSnap = await getDocs(collection(db, 'bases'));
+      this.bases = basesSnap.docs.map(doc => ({ id: doc.id, ...doc.data() })) as any;
+
+      // Load creamers
+      const creamersSnap = await getDocs(collection(db, 'creamers'));
+      this.creamers = creamersSnap.docs.map(doc => ({ id: doc.id, ...doc.data() })) as any;
+
+      // Load syrups
+      const syrupsSnap = await getDocs(collection(db, 'syrups'));
+      this.syrups = syrupsSnap.docs.map(doc => ({ id: doc.id, ...doc.data() })) as any;
+
+      // Set default selections
+      this.currentBase = this.bases[0] || null;
+      this.currentCreamer = this.creamers[0] || null;
+      this.currentSyrup = this.syrups[0] || null;
+    },
     makeBeverage() {},
 
     showBeverage() {},
